8bcaff4f94370186bd769db527ccdf174b8c2313
TytusKolpak <101522301+TytusKolpak@users.noreply.github.com>
2024-05-27 20:28:31 +0200

    Captured gamepad inputs

--- entities/game.go
+++ entities/game.go
@@ -1,6 +1,8 @@
 package entities
 
 import (
+	"fmt"
+	"log"
 	"math"
 	"math/rand"
 	"strconv"
@@ -8,6 +10,7 @@ import (
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+	"github.com/hajimehoshi/ebiten/v2/inpututil"
 )
 
 type Game struct {
@@ -19,6 +22,8 @@ type Game struct {
 	Player           *Player
 	SpawnTime        time.Time
 	EnemiesDestroyed int
+	gamepadIDsBuf    []ebiten.GamepadID
+	gamepadIDs       map[ebiten.GamepadID]struct{}
 }
 
 const (
@@ -37,6 +42,66 @@ func (g *Game) Layout(outsideWidth, outsideHeight int) (int, int) {
 }
 
 func (g *Game) Update() error {
+	if g.gamepadIDs == nil {
+		g.gamepadIDs = map[ebiten.GamepadID]struct{}{}
+	}
+
+	// Manage connecting and disconnecting gamepad(s)
+	g.gamepadIDsBuf = ebiten.AppendGamepadIDs(g.gamepadIDsBuf[:0])
+	for _, id := range g.gamepadIDsBuf {
+		g.gamepadIDs[id] = struct{}{}
+	}
+	for id := range g.gamepadIDs {
+		if inpututil.IsGamepadJustDisconnected(id) {
+			delete(g.gamepadIDs, id)
+		}
+	}
+
+	// Log button events.
+	for id := range g.gamepadIDs {
+		// All joystick inputs [0,1]
+		LSH := ebiten.StandardGamepadAxisValue(id, ebiten.StandardGamepadAxisLeftStickHorizontal)
+		LSV := ebiten.StandardGamepadAxisValue(id, ebiten.StandardGamepadAxisLeftStickVertical)
+		RSH := ebiten.StandardGamepadAxisValue(id, ebiten.StandardGamepadAxisRightStickHorizontal)
+		RSV := ebiten.StandardGamepadAxisValue(id, ebiten.StandardGamepadAxisRightStickVertical)
+		if math.Abs(LSH) > 0.1 {
+			log.Printf("Left horizontal stick value: %v", LSH)
+		}
+		if math.Abs(LSV) > 0.1 {
+			log.Printf("Left vertical stick value: %v", LSV)
+		}
+		if math.Abs(RSH) > 0.1 {
+			log.Printf("Right horizontal stick value: %v", RSH)
+		}
+		if math.Abs(RSV) > 0.1 {
+			log.Printf("Right vertical stick value: %v", RSV)
+		}
+
+		// All trigger inputs [0,1]
+		FBL := ebiten.StandardGamepadButtonValue(id, ebiten.StandardGamepadButtonFrontBottomLeft)
+		FBR := ebiten.StandardGamepadButtonValue(id, ebiten.StandardGamepadButtonFrontBottomRight)
+		if math.Abs(FBL) > 0.1 {
+			log.Printf("Left trigger value: %v", FBL)
+		}
+		if math.Abs(FBR) > 0.1 {
+			log.Printf("Right trigger stick value: %v", FBR)
+		}
+
+		// All buttons being pressed / just pressed 0/1
+		maxButton := ebiten.GamepadButton(ebiten.GamepadButtonCount(id))
+		for b := ebiten.GamepadButton(0); b < maxButton; b++ {
+			if ebiten.IsGamepadButtonPressed(id, b) {
+				log.Printf("button is being pressed: id: %d, button: %d", id, b)
+			}
+			if inpututil.IsGamepadButtonJustPressed(id, b) {
+				log.Printf("button pressed: id: %d, button: %d", id, b)
+			}
+			if inpututil.IsGamepadButtonJustReleased(id, b) {
+				log.Printf("button released: id: %d, button: %d", id, b)
+			}
+		}
+	}
+
 	if gameOver {
 		return nil
 	}
@@ -68,6 +133,67 @@ func (g *Game) Update() error {
 	return nil
 }
 
+// The order of drawing elements on the screen determines their z-index
+func (g *Game) Draw(screen *ebiten.Image) {
+
+	// Do not proceed with logic unless the gamepad is connected
+	if len(g.gamepadIDs) == 0 {
+		ebitenutil.DebugPrint(screen, "Please connect your gamepad.")
+		return
+	}
+
+	// Draw the background
+	opts := &ebiten.DrawImageOptions{}
+	screen.DrawImage(g.BackgroundImg, opts)
+
+	// Create string representing the amount of Enemies destroyed
+	displayEnemiesDestroyed := strconv.Itoa(g.EnemiesDestroyed)
+	stringToDisplay := fmt.Sprintln("Enemies destroyed: " + displayEnemiesDestroyed)
+
+	// Create string representing elapsed time
+	elapsedTime := time.Since(startTime)
+	secondsPassed := int(math.Round(elapsedTime.Seconds()))
+	displaySeconds := strconv.Itoa(secondsPassed % 60)
+	minutesPassed := secondsPassed / 60
+	displayMinutes := strconv.Itoa(minutesPassed % 60)
+	hoursPassed := minutesPassed / 60
+	displayHours := strconv.Itoa(hoursPassed % 24)
+	if !gameOver {
+		displayTime = displayHours + "h " + displayMinutes + "m " + displaySeconds + "s"
+	}
+	stringToDisplay += fmt.Sprintln("Elapsed time: " + displayTime)
+
+	// Display amount of bolts user has
+	stringToDisplay += fmt.Sprintln("Bolts available: " + strconv.Itoa(g.Player.BoltAmount))
+
+	if gameOver {
+		stringToDisplay += fmt.Sprintln("Game over! You've just got gobbled!")
+	}
+
+	// Display on the screen
+	ebitenutil.DebugPrint(screen, stringToDisplay)
+
+	if gameOver {
+		return
+	}
+
+	// Draw all Enemies
+	for _, enm := range g.Enemies {
+		enm.Draw(screen)
+	}
+
+	// Draw all Projectiles
+	for _, p := range g.Projectiles {
+		p.Draw(screen)
+	}
+
+	// Draw the Player
+	g.Player.Draw(screen)
+}
+
+// Custom functions with a Game receiver below
+
+// Logic to spawn an enemy
 func (g *Game) spawnNewEnemy() {
 	// Create a new Enemy
 	enm := &Enemy{
@@ -114,55 +240,4 @@ func (g *Game) checkCollisions() {
 			}
 		}
 	}
-}
-
-// The order of drawing elements on the screen determines their z-index
-func (g *Game) Draw(screen *ebiten.Image) {
-	// Draw the background
-	opts := &ebiten.DrawImageOptions{}
-	screen.DrawImage(g.BackgroundImg, opts)
-
-	// Create string representing the amount of Enemies destroyed
-	displayEnemiesDestroyed := strconv.Itoa(g.EnemiesDestroyed)
-	stringToDisplay := "Enemies destroyed: " + displayEnemiesDestroyed + ". "
-
-	// Create string representing elapsed time
-	elapsedTime := time.Since(startTime)
-	secondsPassed := int(math.Round(elapsedTime.Seconds()))
-	displaySeconds := strconv.Itoa(secondsPassed % 60)
-	minutesPassed := secondsPassed / 60
-	displayMinutes := strconv.Itoa(minutesPassed % 60)
-	hoursPassed := minutesPassed / 60
-	displayHours := strconv.Itoa(hoursPassed % 24)
-	if !gameOver {
-		displayTime = displayHours + "h " + displayMinutes + "m " + displaySeconds + "s"
-	}
-	stringToDisplay += "Elapsed time: " + displayTime + ". "
-
-	// Display amount of bolts user has
-	stringToDisplay += "Bolts available: " + strconv.Itoa(g.Player.BoltAmount)
-
-	if gameOver {
-		stringToDisplay += ". Game over! You've just got gobbled!"
-	}
-
-	// Display on the screen
-	ebitenutil.DebugPrint(screen, stringToDisplay)
-
-	if gameOver {
-		return
-	}
-
-	// Draw all Enemies
-	for _, enm := range g.Enemies {
-		enm.Draw(screen)
-	}
-
-	// Draw all Projectiles
-	for _, p := range g.Projectiles {
-		p.Draw(screen)
-	}
-
-	// Draw the Player
-	g.Player.Draw(screen)
 }
